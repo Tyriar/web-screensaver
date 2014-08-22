746c27bab670f4455e92d78c40c1da5f2f6763f1
Daniel Imms <tyriar@tyriar.com>
2014-08-22 03:52:13 -0700

    Initial js file commit

---
+++ web-screensaver.js
@@ -0,0 +1,54 @@
+var webScreensaver = (function () {
+  var module = {};
+
+  var timeout;
+
+  var config = {
+    url: undefined,
+    waitTime: 60,
+    script: undefined
+  };
+
+  function restart() {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(module.downloadThenStart, waitTime);
+  }
+
+  module.register = function (url, waitTime) {
+    if (!url) {
+      return;
+    }
+
+    config.url = url;
+    if (waitTime) {
+      config.waitTime = waitTime;
+    }
+    restart();
+  };
+
+  module.downloadThenStart = function () {
+    if (!config.script) {
+      // TODO: Download script, then call show after setting script
+      config.script = 'alert("time")';
+      module.show();
+    } else {
+      module.show();
+    }
+  };
+
+  module.show = function () {
+    var src = document.createElement('script');
+    src.setAttribute('type', 'text/javascript');
+    //src.setAttribute('src', config.url);
+    src.textContent = config.script;
+    document.head.appendChild(src);
+    // remove the script after it's run?
+    // set a variable based on the plugin name?
+    // leave 'var plugin = ' part up to the framework?
+  }
+
+  return module;
+})();
